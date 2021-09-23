c98756988ebe0a0654a6951a67548da1943c6eb5
Sayran Felix <88841039+SayranFelix@users.noreply.github.com>
2021-09-23 07:44:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,1 @@
-const express = require('express');
-const app = express();
-
 alert("Uepa! 😄 Bem-vindo ao meu portfólio, saiba mais sobre mim aqui.")
-
-function EnglishCategory() {
-  
-  app.get('/', (req, res) => {
-    res.render('englishPage')
-  })
-  
-}
